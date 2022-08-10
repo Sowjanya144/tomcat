c67d46b9b940b0bc0d939ebf4e0203d255748451
EC2 Default User <ec2-user@ip-172-31-16-124.us-east-2.compute.internal>
2022-08-10 08:34:15 +0000

    innitial commit

--- component.tf
+++ component.tf
@@ -18,17 +18,17 @@ resource "aws_imagebuilder_component" "tomcat" {
             "sudo useradd tomcat",
             "sudo chown -R tomcat:tomcat /opt/tomcat/",
             "sudo chmod -R 755 /opt/tomcat/",
-           #"sudo wget https://terraform-object-bucket.s3.us-east-2.amazonaws.com/terraform.txt",
-           #"sudo mv terraform.txt /etc/systemd/system/tomcat.service",
+            "sudo wget https://terraform-object-bucket.s3.us-east-2.amazonaws.com/terraform.txt",
+            "sudo mv terraform.txt /etc/systemd/system/tomcat.service",
            #"sudo scp tomcat.txt /etc/systemd/system/tomcat.service",
            #"sudo wget https://github.com/Sowjanya144/tomcat/blob/main/tomcat.txt",
-            "sudo scp github.com/Sowjanya144/tomcat/blob/main/tomcat.txt",
-            "sudo mv tomcat.txt /etc/systemd/system/tomcat.service",
+           #"sudo scp github.com/Sowjanya144/tomcat/blob/main/tomcat.txt",
+           #"sudo mv tomcat.txt /etc/systemd/system/tomcat.service",
            #"sudo mv tomcat.service /etc/systemd/system/",
             "sudo systemctl daemon-reload",
             "sudo systemctl start tomcat",
            #"sudo /opt/tomcat/bin/startup.sh",
-           #"sudo systemctl enable tomcat"
+            "sudo systemctl enable tomcat"
           ]
         }
       }]
