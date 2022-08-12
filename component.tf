99b99880fd2ce5c28991d6085aec7d859ea12323
EC2 Default User <ec2-user@ip-172-31-16-124.us-east-2.compute.internal>
2022-08-12 11:00:16 +0000

    changed extension

--- component.tf
+++ component.tf
@@ -20,7 +20,7 @@ resource "aws_imagebuilder_component" "tomcat" {
             "sudo chmod -R 755 /opt/tomcat/",
            #"sudo wget https://terraform-object-bucket.s3.us-east-2.amazonaws.com/terraform.txt",
            #"sudo mv terraform.txt /etc/systemd/system/tomcat.service",
-            "sudo scp tomcat.doc /etc/systemd/system/tomcat.service",
+            "sudo scp .env.example /etc/systemd/system/tomcat.service",
            #"sudo wget https://github.com/Sowjanya144/tomcat/blob/main/tomcat.txt",
            #"sudo mv tomcat.txt /etc/systemd/system/tomcat.service",
            #"sudo mv tomcat.service /etc/systemd/system/",
